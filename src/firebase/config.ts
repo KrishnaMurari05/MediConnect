93d39a955df271fbe457342fbaae4e4c992f569d
It's MiGhT GaMer <itsmightgamer@gmail.com>
2026-02-11 18:30:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7871386470-935d9",
+  "appId": "1:429138650885:web:52375a5f0e2d00601fb96c",
+  "apiKey": "AIzaSyAWDd-feg2icyYRJmMJz1tNRw7rCaosbSM",
+  "authDomain": "studio-7871386470-935d9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "429138650885"
+};
