2b33bb82c9645cda0a78ced1d32d95c9e4ffb561
It's MiGhT GaMer <itsmightgamer@gmail.com>
2026-02-11 18:36:15 +0000

    Try fixing this error: `Runtime Error: useFirebase must be used within a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type {Metadata} from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { FirebaseClientProvider } from '@/firebase';
+import { Toaster } from '@/components/ui/toaster';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -21,7 +23,10 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.variable}>
       <body className="font-body antialiased selection:bg-primary/20">
-        {children}
+        <FirebaseClientProvider>
+          {children}
+          <Toaster />
+        </FirebaseClientProvider>
       </body>
     </html>
   );
